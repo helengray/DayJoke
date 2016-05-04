91ed16cd24ec644c97bc27def884528238d183aa
lixiaowei <lixiaowei@91yong.com>
2016-05-04 15:44:27 +0800

    资源修改

---
+++ app/src/main/java/com/helen/dayjoke/ui/adapter/JokePagerAdapter.java
@@ -0,0 +1,41 @@
+package com.helen.dayjoke.ui.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 李晓伟 on 2016/4/29.
+ *
+ */
+public class JokePagerAdapter extends FragmentPagerAdapter{
+    private final List<Fragment> mFragments = new ArrayList<>();//添加的Fragment的集合
+    private final List<String> mFragmentsTitles = new ArrayList<>();//每个Fragment对应的title的集合
+
+    public JokePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String fragmentTitle) {
+        mFragments.add(fragment);
+        mFragmentsTitles.add(fragmentTitle);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentsTitles.get(position);
+    }
+}
